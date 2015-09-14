76ca1b3e6426efd3d404b1dd42bbc9e28e0eda06
Jon <powers.j.email@gmail.com>
2015-09-14 17:20:09 -0400

    Camera Movement
    Offset the camera from the player.

---
+++ CameraMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+
+	// The main player of the game
+	public GameObject player;
+	// Offset of the camera to the player
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position;
+	}
+	// Move the camera off of the player
+	void LateUpdate(){
+		transform.position = player.transform.position + offset;
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
