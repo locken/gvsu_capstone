265740865ee9e91d683b5b8f77a721dbbe17b03b
largentj <largentj@outlook.com>
2015-09-20 15:13:03 -0400

    Basic attack/block
    Triggers an attack with a weapon and a block with a shield.

---
+++ Assets/cs_core/character/attackblock.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class attackblock : MonoBehaviour {
+
+    Animator anim;
+    Quaternion rot;
+
+	void OnTriggerEnter2D(Collider2D other)
+    {
+        Destroy (other.gameObject);
+    }
+
+    // Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            anim.SetTrigger("Attack");
+        }
+
+        if (Input.GetMouseButton(1))
+        {
+            anim.SetBool("Block", true);
+        }
+        else
+            anim.SetBool("Block", false);
+	}
+}
