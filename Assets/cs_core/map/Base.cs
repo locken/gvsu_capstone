bafe87647cd85474a5d8e0b374ced67868c276d7
Nolan Locke <locken@mail.gvsu.edu>
2015-10-04 10:51:28 -0400

    Adding map scripts (unfinished), and test scene

---
+++ Assets/cs_core/map/Base.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Base : MonoBehaviour {
+
+    //object to contain our 'floor'
+    public GameObject floor;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/cs_core/map/Map.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Map: MonoBehaviour {
+    Random r = new Random();
+    int length, height;
+    int seed;
+
+	// Use this for initialization
+	void Start () {
+        seed = 69;
+        Random.seed = seed;
+        length = Random.Range(15, 25);
+        height = Random.Range(20, 30);
+        Debug.Log("Seed: " + seed);
+        Debug.Log("Map Size: " + length + 'x' + height);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
