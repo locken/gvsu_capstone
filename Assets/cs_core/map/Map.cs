c0cded3d9841165e702be2309bab61eb44962f4f
Nolan Locke <locken@mail.gvsu.edu>
2015-10-04 17:40:18 -0400

    Progromatically inserting map.

--- Assets/cs_core/map/Map.cs
+++ Assets/cs_core/map/Map.cs
@@ -2,11 +2,13 @@
 using System.Collections;
 
 public class Map: MonoBehaviour {
+
+    GameObject ground;
     Random r = new Random();
-    int length, height;
+    float length, height;
     int seed;
     Walls roomWalls;
-    SpriteRenderer s;
+    Object[] mapSprites;
 
     public class Walls {
         public GameObject topWall = new GameObject();
@@ -21,9 +23,9 @@ public class Map: MonoBehaviour {
             wallThickness = 3.0f;
             //uses sprites width to create new length of wall
             topWall.transform.position = new Vector3(0, height/2, layer);
-            topWall.transform.localScale = new Vector3(length * 4, wallThickness);
+            topWall.transform.localScale = new Vector3(1, 1);
             bottomWall.transform.position = new Vector3(0, -height / 2, layer);
-            bottomWall.transform.localScale = new Vector3(length * 4, wallThickness);
+            bottomWall.transform.localScale = new Vector3(length, wallThickness);
 
             rock.transform.position = new Vector3(0, 0, layer);
             rock.transform.localScale = new Vector3(3, 3, 1);
@@ -61,17 +63,66 @@ public class Map: MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        s = gameObject.GetComponent<SpriteRenderer>();
+
+        mapSprites = Resources.LoadAll("map");
+        ground = new GameObject();
+
+        length = 16;
+        height = 6;
+
+        GameObject rock = new GameObject();
+        SpriteRenderer rockSprite = rock.AddComponent<SpriteRenderer>();
+        rockSprite.sprite = (Sprite)mapSprites[7];
+        BoxCollider2D rockBody = rock.AddComponent<BoxCollider2D>();
+
+
+        createGround(ground);
+
+        SpriteRenderer groundSprite = ground.GetComponent<SpriteRenderer>();
+
+        float tileLength = groundSprite.bounds.size.x;
+        float tileWidth = groundSprite.bounds.size.y;
+
+        ground.transform.position = new Vector3(-length/2, -height/2 - 0.5f, 0);
+        
         //our "random" seed heh
         seed = 69;
         Random.seed = seed;
-        length = Random.Range(15, 25);
-        height = Random.Range(20, 30);
+        //length = Random.Range(15, 25);
+        //height = Random.Range(20, 30);
 
-        roomWalls = new Walls(s.bounds.size.x, s.bounds.size.y, -10);
-        Debug.Log("Seed: " + seed);
-        Debug.Log("Map Size: " + length + 'x' + height);
+
+        //roomWalls = new Walls(length, height, -10);
+        GameObject tracked = ground;
+        GameObject newGround;
+        for (int i = 0; i <= height + 1; i++ )
+        {
+            if(i > 0)
+            {
+               newGround = (GameObject)Instantiate(tracked, new Vector3(ground.transform.position.x, ground.transform.position.y+ i), Quaternion.identity);
+                tracked = newGround;
+            }
+
+            for (int j = 1; j <= length; j++)
+            {
+                newGround = (GameObject)Instantiate(tracked, tracked.transform.position + Vector3.right, Quaternion.identity);
+                tracked = newGround;
+            }
+            
+        }
+
     }
+
+
+    SpriteRenderer createGround(GameObject ground)
+    {
+        SpriteRenderer groundSprite = ground.AddComponent<SpriteRenderer>();
+
+        groundSprite.sprite = (Sprite)mapSprites[3];
+
+        return groundSprite;
+    }
+
 	
 	// Update is called once per frame
 	void Update () {
