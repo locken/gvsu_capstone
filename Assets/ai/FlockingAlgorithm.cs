7a675052f378b619e968e632bf96c41791d42197
Gerad <wegenerg@mail.gvsu.edu>
2015-10-05 18:05:12 -0400

    Commit movements and flocking algorithm script

---
+++ Assets/ai/FlockingAlgorithm.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlockingAlgorithm : MonoBehaviour {
+	public float seperation;
+	public float alignnment; 
+	public float cohesion; 
+	public static bool collision;
+	public static float x;
+	public static float y;
+
+	void Start()
+	{
+		CircleCollider2D collider = GetComponent<CircleCollider2D> ();
+		collider.radius = seperation;
+	}
+
+	void OnTriggerEnter2D(Collider2D  other) {
+		movements.collision = true;
+		/*Vector2 enemyPosition = GetComponent<Rigidbody2D> ().position;
+		float x;
+		float y;
+		if (enemyPosition.x >= other.attachedRigidbody.position.x) {
+			x = -1;
+		} else {
+			x = 1;
+		}
+		if (enemyPosition.y >= other.attachedRigidbody.position.y) {
+			y = -1;
+		} else {
+			y = 1;
+		}
+
+		Vector2 movement = new Vector3 (x, y);
+		float speed = movements.GetSpeed ();
+		GetComponent<Rigidbody2D> ().velocity = movement * speed;
+		GetComponent<Rigidbody2D> ().position = new Vector2
+			(
+				Mathf.Clamp (GetComponent<Rigidbody2D> ().position.x, -5.0f, 5.0f), 
+				Mathf.Clamp (GetComponent<Rigidbody2D> ().position.y, -5.0f, 5.0f)
+				);*/
+		//Debug.Log ("Trigger Hit");
+		/*if (movements.getEngaged()) {
+			Vector2 movement = new Vector3 (moveHorizontal, moveVertical);
+			GetComponent<Rigidbody2D> ().velocity = movement * speed;
+		}*/
+	}
+
+	void OnTriggerExit2D(Collider2D  other) {
+		movements.collision = false;
+	}
+}
--- Assets/ai/movements.cs
+++ Assets/ai/movements.cs
@@ -9,41 +9,49 @@ public class movements : MonoBehaviour
 	public int x;
 	public int y;
 	public float now;
-	
+	private bool engaged; 
+	public static bool collision;
+	public static float FlockingSpeed;
 	// Use this for initialization
 	void Start ()
 	{
 		transform.rotation = rot;
 		transform.eulerAngles = new Vector3 (0, 0, transform.eulerAngles.z);
 		now = Time.time + 2.0f;
-		x = 1;
-		y = 1;
+		x = Random.Range (-1, 2);
+		y = Random.Range (-1, 2);
+		engaged = false;
+		FlockingSpeed = speed;
+		collision = false;
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		if (Time.time > now) {
-			x = Random.Range (-1, 2);
-			y = Random.Range (-1, 2);
-			
-			now = Time.time + 2.0f;
-			Debug.Log ("X is: " + x);
-			Debug.Log ("Y is: " + y);
-		}
-		float moveHorizontal = (float)x;
-		float moveVertical = (float)y;
-		Debug.Log ("MoveH is: " + x);
-		Debug.Log ("MoveY is: " + y);
-		Vector2 movement = new Vector3 (moveHorizontal, moveVertical);
-		GetComponent<Rigidbody2D>().velocity = movement * speed;
+		if (!collision) {
+			if (Time.time > now) {
+				x = Random.Range (-1, 2);
+				y = Random.Range (-1, 2);
+
+				now = Time.time + Random.Range(1.0f, 3.0f);
+			}
+			float moveHorizontal = (float)x;
+			float moveVertical = (float)y;
+			Vector2 movement = new Vector3 (moveHorizontal, moveVertical);
+			GetComponent<Rigidbody2D> ().velocity = movement * speed;
 		
-		GetComponent<Rigidbody2D>().position = new Vector2
+			GetComponent<Rigidbody2D> ().position = new Vector2
 			(
-				Mathf.Clamp (GetComponent<Rigidbody2D>().position.x, -5.0f, 5.0f), 
-				Mathf.Clamp (GetComponent<Rigidbody2D>().position.y,  -5.0f, 5.0f)
-				);
+				Mathf.Clamp (GetComponent<Rigidbody2D> ().position.x, -5.0f, 5.0f), 
+				Mathf.Clamp (GetComponent<Rigidbody2D> ().position.y, -5.0f, 5.0f)
+			);
 		
-		//GetComponent<Rigidbody>().rotation = Quaternion.Euler (0.0f, 0.0f, GetComponent<Rigidbody>().velocity.x * -tilt);
+			//GetComponent<Rigidbody>().rotation = Quaternion.Euler (0.0f, 0.0f, GetComponent<Rigidbody>().velocity.x * -tilt);
+		}
+	}
+	
+	public static float GetSpeed()
+	{
+		return FlockingSpeed;
 	}
 }
