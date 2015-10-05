6c89520d123f46255413804046913daf890e012a
Lucas Braun <braunlu@mail.gvsu.edu>
2015-10-05 17:39:21 -0400

    Items and Inventory
    Created abstract item class, extended it into a weapon, armor,
    consumable, and other item classes. Set up classes for inventory and
    player inventory.

--- Assets/cs_core/inventory/Inventory.cs
+++ Assets/cs_core/inventory/Inventory.cs
@@ -3,9 +3,22 @@ using System.Collections;
 
 public class Inventory : MonoBehaviour {
 
-    Item[] items;
+    public List<Items> items = new List<Items>();
 
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    void Update()
+    {
 
+    }
+
+    [System.Serializable]
+public class Items
+{
+    public Transform itemTransform;
+}
 
     
 }
---
+++ Assets/cs_core/inventory/PlayerInventory.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerInventory : MonoBehaviour {
+
+    public Inventory itemManager;
+    public List<playerItems> items = new List<playerItems>();
+
+    void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+
+    public void addItem(int itemId, int itemQuantity)
+    {
+        for(int i=0; i < itemManager.items.count; i++)
+        {
+            if(itemManager.items[i].itemTransform.getComponent<Item>().id == itemId)
+            {
+                playerItems itemsToAdd = new playerItems(itemManager.items[i].itemTransform.getComponent<Item>(), itemQuantity);
+                items.Add(itemsToAdd);
+            }
+        }
+    }
+
+    [System.Serializable]
+    public class playerItems
+    {
+        public Item item;
+        public int itemQuantity;
+
+        public playerItems(Item item, int itemQuantity)
+        {
+            this.item = item;
+            this.itemQuantity = itemQuantity;
+        }
+    }
+}
