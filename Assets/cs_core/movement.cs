6802e0c884b4eea72b7f46be058fe77be7af360a
largentj <largentj@outlook.com>
2015-10-09 17:29:34 -0400

    sprite and movement changes
    changed size and pixels per unit on sword, shield, and player.  Fixed
    movement script to move based on world and accelerate to maxspeed 5.

--- Assets/cs_core/movement.cs
+++ Assets/cs_core/movement.cs
@@ -3,18 +3,13 @@ using System.Collections;
 
 public class movement : MonoBehaviour {
 
-    public float speed;
-    int x, y;
-    float move1, move2;
+    public float speed, maxspeed;
     public Quaternion rot;
 
 	// Use this for initialization
 	void Start () {
-        speed = 5;
-        move1 = Time.deltaTime * speed;
-        move2 = 0;
-        x = 1;
-        y = 1;
+        speed = 0;
+        maxspeed = 5;
         rot = Quaternion.LookRotation(new Vector3(0, 0, 0), Vector3.up);
         transform.rotation = rot;
         transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z);
@@ -23,60 +18,51 @@ public class movement : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-        //var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        //Quaternion rot = Quaternion.LookRotation(transform.position - mousePosition, Vector3.forward);
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Translate(-1* x * move1, -1* y * move2, 0f);
-        }
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            rot = Quaternion.LookRotation(new Vector3(0, 0, 270), Vector3.left);
-            move1 = 0;
-            move2 = Time.deltaTime * speed;
-            x = 1;
-            y = -1;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.Translate(x * move1, y * move2, 0f);
-        }
-        if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            rot = Quaternion.LookRotation(new Vector3(0, 0, 90), Vector3.right);
-            move1 = 0;
-            move2 = Time.deltaTime * speed;
-            x = -1;
-            y = 1;
-        }
-        if (Input.GetKey(KeyCode.W))
-        {
-            transform.Translate(x * move2, y * move1, 0f);
-        }
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             rot = Quaternion.LookRotation(new Vector3(0, 0, 0), Vector3.up);
-            move1 = Time.deltaTime * speed;
-            move2 = 0;
-            x = 1;
-            y = 1;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.Translate(-1* x * move2, -1 * y * move1, 0f);
         }
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             rot = Quaternion.LookRotation(new Vector3(0, 0, 180), Vector3.down);
-            move1 = Time.deltaTime * speed;
-            move2 = 0;
-            x = -1;
-            y = -1;
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            rot = Quaternion.LookRotation(new Vector3(0, 0, 90), Vector3.right);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            rot = Quaternion.LookRotation(new Vector3(0, 0, 270), Vector3.left);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            if (speed < maxspeed)
+                speed += .2f;
+            transform.Translate(-1 * speed * Time.deltaTime,0f, 0f,Space.World);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            if (speed < maxspeed)
+                speed += .2f;
+            transform.Translate(speed*Time.deltaTime,0f, 0f,Space.World);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            if (speed < maxspeed)
+                speed += .2f;
+            transform.Translate(0f, speed*Time.deltaTime, 0f,Space.World);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            if (speed < maxspeed)
+                speed += .2f;
+            transform.Translate(0f, -1*speed*Time.deltaTime, 0f,Space.World);
+        }
+        if(!(Input.GetKey(KeyCode.A)|| Input.GetKey(KeyCode.D)|| Input.GetKey(KeyCode.W)|| Input.GetKey(KeyCode.S)))
+        {
+            speed = 0;
         }
         transform.rotation = rot;
         transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z);
-        
-        //transform.Translate(Input.GetAxis("Horizontal")*Time.deltaTime*speed, Input.GetAxis("Vertical")*Time.deltaTime*speed, 0f);
     }
 }
