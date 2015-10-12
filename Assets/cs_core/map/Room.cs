394bc8d77123eb8f4c991f8ce93eb8fd8aa3c6bc
Nolan Locke <itsknolan@yahoo.com>
2015-10-12 05:54:22 -0400

    Room comment added, deleted build file

--- Assets/cs_core/map/Room.cs
+++ Assets/cs_core/map/Room.cs
@@ -2,14 +2,26 @@
 using System.Collections;
 
 public class Room : MonoBehaviour {
+    /***
+    Things we will need for an individual room:
 
-	// Use this for initialization
-	void Start () {
-	
+    1. length, width
+
+    2. exits (coordinates? sides?) exits = [ [x,y], [x2,y2], [x3,y3], [x4,y4] ]
+    3. center point for triangulation
+
+
+    ***/
+
+
+
+    // Use this for initialization
+    void Start () {
+	    
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+	    
 	}
 }
