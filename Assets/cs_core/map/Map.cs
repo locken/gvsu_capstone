d050b7bfe6b8a3876634aecff825c53cfceff95c
Nolan Locke <itsknolan@yahoo.com>
2015-10-30 20:50:17 -0400

    Map changes. Walls in progress.

--- Assets/cs_core/map/Map.cs
+++ Assets/cs_core/map/Map.cs
@@ -3,124 +3,97 @@ using System.Collections;
 
 public class Map : MonoBehaviour {
 
-    GameObject ground;
+    GameObject baseTile;
     Random r = new Random();
     float length, height;
     int seed;
-    Walls roomWalls;
     Object[] mapSprites;
-
-    public class Walls {
-        public GameObject topWall = new GameObject();
-        public GameObject bottomWall = new GameObject();
-        public GameObject rock = new GameObject();
-        float wallThickness;
-        Object[] mapSprites;
-
-        public Walls(float length, float height, float layer) {
-            mapSprites = Resources.LoadAll("map");
-            //set the wall thickness here
-            wallThickness = 3.0f;
-            //uses sprites width to create new length of wall
-            topWall.transform.position = new Vector3(0, height / 2, layer);
-            topWall.transform.localScale = new Vector3(1, 1);
-            bottomWall.transform.position = new Vector3(0, -height / 2, layer);
-            bottomWall.transform.localScale = new Vector3(length, wallThickness);
-
-            rock.transform.position = new Vector3(0, 0, layer);
-            rock.transform.localScale = new Vector3(3, 3, 1);
-
-            createWall(topWall);
-            createWall(bottomWall);
-            createRock(rock);
-            topWall.name = "topWall";
-            bottomWall.name = "bottomWall";
-            rock.name = "rock";
-            Debug.Log("Top and Bottom Wall Size: " + length + "x" + wallThickness);
-            Debug.Log("Z Axis: " + layer);
-        }
-
-        SpriteRenderer createWall(GameObject wall)
-        {
-            //add spriterenderer
-            SpriteRenderer wallSprite = wall.AddComponent<SpriteRenderer>();
-            wallSprite.sprite = (Sprite)mapSprites[3];
-
-            BoxCollider2D wallBody = wall.AddComponent<BoxCollider2D>();
-
-            return wallSprite;
-        }
-
-        SpriteRenderer createRock(GameObject rock)
-        {
-            SpriteRenderer rockSprite = rock.AddComponent<SpriteRenderer>();
-            rockSprite.sprite = (Sprite)mapSprites[5];
-
-            BoxCollider2D rockBody = rock.AddComponent<BoxCollider2D>();
-            return rockSprite;
-        }
-    }
+    ArrayList floorTiles;
+    string tileset;
 
     // Use this for initialization
     void Start() {
-
-        mapSprites = Resources.LoadAll("map");
-        ground = new GameObject();
-
-        //length = 16;
-        //height = 6;
-
-        GameObject rock = new GameObject();
-        SpriteRenderer rockSprite = rock.AddComponent<SpriteRenderer>();
-        rockSprite.sprite = (Sprite)mapSprites[17];
-        BoxCollider2D rockBody = rock.AddComponent<BoxCollider2D>();
-
-
-        createGround(ground);
-
-        SpriteRenderer groundSprite = ground.GetComponent<SpriteRenderer>();
-
-        float tileLength = groundSprite.bounds.size.x;
-        float tileWidth = groundSprite.bounds.size.y;
-
-        //ground.transform.position = new Vector3(-length/2, -height/2 - 0.5f, 0);
-
-        //our "random" seed heh
         seed = System.DateTime.Now.Second;
         Random.seed = seed;
-        //length = Random.Range(15, 25);
-        //height = Random.Range(20, 30);
-        //length = 16;
+        this.transform.name = "room";
+
+        baseTile = new GameObject();
+
+        //ARRAY SIZE = NUMBER OF FOLDERS IN Resources/map
+        string[] tilesets = new string[1];
+        tilesets[0] = "map/desert/";
+        int tsIndex= Random.Range(0, tilesets.Length);
+        tileset = tilesets[tsIndex];
+
+
+
+        /*** LOAD SPRITES ***/
+        floorTiles = new ArrayList();
+
+        Sprite wallv = Resources.Load<Sprite>(tileset + "wallv");
+        Sprite wallh = Resources.Load<Sprite>(tileset + "wallh");
+        Sprite walltr = Resources.Load<Sprite>(tileset + "walltr");
+        Sprite wallbr = Resources.Load<Sprite>(tileset + "wallbr");
+        Sprite walltl = Resources.Load<Sprite>(tileset + "walltl");
+        Sprite wallbl = Resources.Load<Sprite>(tileset + "wallbl");
+        Sprite floor01 = Resources.Load<Sprite>(tileset + "floor01");
+        Sprite floor02 = Resources.Load<Sprite>(tileset + "floor02");
+        Sprite floor03 = Resources.Load<Sprite>(tileset + "floor03");
+        Sprite floor04 = Resources.Load<Sprite>(tileset + "floor04");
+        Sprite floor05 = Resources.Load<Sprite>(tileset + "floor05");
+
+        floorTiles.Add(floor01);
+        floorTiles.Add(floor02);
+        floorTiles.Add(floor03);
+        floorTiles.Add(floor04);
+        floorTiles.Add(floor05);
+        /*** END LOAD ***/
+
+        //GameObject rock = createCollidable("rock01"/);
+
+        getFloorSprite(baseTile);
+
+        SpriteRenderer floorSprite = baseTile.GetComponent<SpriteRenderer>();
+        float tileLength = floorSprite.bounds.size.x;
+        float tileWidth = floorSprite.bounds.size.y;
+
         height = Random.Range(10, 30);
         length = Random.Range(10, 30);
-        ground.transform.position = new Vector3(-length / 2, -height / 2 - 0.5f, 0);
+        baseTile.transform.position = new Vector3(-length / 2, -height / 2 - 0.5f, 0);
 
-
-        //roomWalls = new Walls(length, height, -10);
-        GameObject tracked = ground;
-        GameObject newGround;
-        for (int i = 0; i <= height + 1; i++)
+        GameObject currentTile = baseTile;
+        GameObject nextTile;
+        for (int y = 0; y <= height + 1; y++)
         {
-            //createGround(ground);
-            if (i > 0)
-            {
-                newGround = (GameObject)Instantiate(tracked, new Vector3(ground.transform.position.x, ground.transform.position.y + i), Quaternion.identity);
-                tracked = newGround;
-            }
+            nextTile = new GameObject();
+            nextTile.transform.position =  new Vector3(baseTile.transform.position.x, baseTile.transform.position.y + y, 0.0f);  
+            currentTile = nextTile;
 
-            for (int j = 1; j <= length; j++)
+            for (int x = 0; x <= length; x++)
             {
-                //createGround(newGround);
-                newGround = (GameObject)Instantiate(tracked, tracked.transform.position + Vector3.right, Quaternion.identity);
-                createGround(newGround);
-                tracked = newGround;
-                //yield return new WaitForSeconds(0.001F);
-                //StartCoroutine(Wait(0.065F));
-            }
 
+                if (x == 0 | x == length)
+                {
+                    GameObject wall = createCollidable("wallv", baseTile, x, y);
+                }
+                else if (y == 0 | y == height)
+                {
+                    GameObject wall = createCollidable("wallh", baseTile, x, y);
+                }
+                else
+                {
+                    nextTile = (GameObject)Instantiate(currentTile, currentTile.transform.position + Vector3.right, Quaternion.identity);
+                    nextTile.transform.parent = this.transform;
+                    getFloorSprite(nextTile);
+                    currentTile = nextTile;
+                }
+
+
+                
+            }
         }
-
     }
+
     //for the wait function
     IEnumerator Wait(float duration)
     {
@@ -131,29 +104,36 @@ public class Map : MonoBehaviour {
         Debug.Log("End Wait() function and the time is: " + Time.time);
     }
 
-    SpriteRenderer createGround(GameObject ground)
+    SpriteRenderer getFloorSprite(GameObject floorObj)
     {
-        SpriteRenderer groundSprite = ground.GetComponent<SpriteRenderer>();
-        if (groundSprite == null)
+        SpriteRenderer sr = floorObj.GetComponent<SpriteRenderer>();
+        if (sr == null)
         {
-            groundSprite = ground.AddComponent<SpriteRenderer>();
+            sr = floorObj.AddComponent<SpriteRenderer>();
         }
-        groundSprite.sprite = (Sprite)mapSprites[GetFloorNum()];
-        //int temp = GetFloorNum();
-        return groundSprite;
+        sr.sprite = (Sprite)floorTiles[Random.Range(0,floorTiles.Count)];
+        return sr;
     }
-    int GetFloorNum()
-    {
-        int temp = 2;
-        while (temp % 2 == 0) {
-            temp = Random.Range(3, 15);
-        }
-        Debug.Log(temp);
-        return temp;
-    }
+
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    GameObject createCollidable(string name, GameObject baseTile, int x, int y)
+    {
+        GameObject retVal = new GameObject();
+        retVal.transform.name = name;
+        retVal.transform.parent = this.transform;
+        retVal.transform.position = new Vector3(baseTile.transform.position.x + x, baseTile.transform.position.y + y, 0.0f);
+        
+        
+       
+
+        SpriteRenderer retSprite = retVal.AddComponent<SpriteRenderer>();
+        retSprite.sprite = Resources.Load<Sprite>(tileset + name);
+        BoxCollider2D collider = retVal.AddComponent<BoxCollider2D>();
+        return retVal;
+    }
 }
