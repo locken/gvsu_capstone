6493f4a79c3d9d9903fe1dc154bdc498a4850174
Jon <powers.j.email@gmail.com>
2015-11-02 13:11:57 -0500

    pause the game with p

---
+++ Assets/cs_instance/ui/PauseMenu.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseMenu : MonoBehaviour {
+    bool paused;
+	// Use this for initialization
+	void Start () {
+        paused = false;
+	}
+
+    void PauseGame()
+    {
+        Time.timeScale = 0;
+        paused = true;
+    }
+
+    void OnGUI()
+    {
+        if (paused)
+        {
+            if (GUI.Button(new Rect(200, 100, 80, 50), "Unpause"))
+            {
+                print("Unpause clicked");
+                UnPauseGame();
+            }
+            if (GUI.Button(new Rect(200, 150, 80, 50), "Save Game"))
+            {
+                print("Save clicked");
+
+            }
+            if (GUI.Button(new Rect(200, 200, 80, 50), "Main Menu"))
+            {
+                print("Main Menu clicked");
+                UnPauseGame();
+                Application.LoadLevel(0);
+            }
+        }
+    }
+
+
+
+    void UnPauseGame()
+    {
+        Time.timeScale = 1;
+        paused = false;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown("p"))
+        {
+            PauseGame();
+        }
+	}
+}
